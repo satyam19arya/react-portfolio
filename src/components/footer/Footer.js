1d9afead259126395fb81457f3175036a05a7d58
Satyam <satyam19arya@gmail.com>
2022-12-04 14:51:48 +0530

    changes done

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import './Footer.css';
 
+import { BsLinkedin } from 'react-icons/bs';
+import { BsGithub } from 'react-icons/bs';
+import { BsInstagram } from 'react-icons/bs';
+
+
 const Footer = () => {
     return(
         <div>
-            <section  id='footer'>
-                Footer
-            </section>
+            <footer>
+                <a href="#home" className="footer_logo">Satyam Arya</a>
+
+                <ul className="permalinks">
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#experience">Experience</a></li>
+                    <li><a href="#services">Services</a></li>
+                    <li><a href="#portfolio">Portfolio</a></li>
+                    <li><a href="#testimonials">Testimonials</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+
+                <div className="footer_socials">
+                    <a href="https://www.linkedin.com/in/satyam-arya-101764205/" target="__blank"><BsLinkedin /></a>
+                    <a href="https://github.com/satyam19arya" target="__blank"><BsGithub /></a>
+                    <a href="https://www.instagram.com/satyam19arya/" target="__blank"><BsInstagram /></a>
+                </div>
+
+                <div className="footer_copyright">
+                    <small>Copyright &copy; 2023 Satyam Arya | All rights reserved</small>
+                </div>
+            </footer>
         </div>
     );
 }
