56dfe789050e0d57648bbccebe54184f8196f4c6
Satyam <satyam19arya@gmail.com>
2022-12-29 22:02:56 +0530

    layout done

--- src/components/header/Header_socials.js
+++ src/components/header/Header_socials.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BsLinkedin } from 'react-icons/bs';
 import { BsGithub } from 'react-icons/bs';
 import { BsInstagram } from 'react-icons/bs';
+import { MdOutlineVerified } from 'react-icons/md';
 
 
 const Headersocials = () => {
@@ -10,6 +11,7 @@ const Headersocials = () => {
         <a href="https://www.linkedin.com/in/satyam-arya-101764205/" target="__blank"><BsLinkedin/></a>
         <a href="https://github.com/satyam19arya" target="__blank"><BsGithub/></a>
         <a href="https://www.instagram.com/satyam19arya/" target="__blank"><BsInstagram/></a>
+        <a href="https://www.credly.com/users/satyam-arya.f0a430e7/badges" target="__blank"><MdOutlineVerified/></a>
     </div>
   )
 }
